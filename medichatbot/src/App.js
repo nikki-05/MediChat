768e32e50559edaff12c3c5b682c112419644df8
Nikita <nikitatewari1633@gmail.com>
2024-10-13 22:01:15 +0530

    create medical chatbot
    This chatbot aims to provide medical assistance to students

---
+++ medichatbot/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Bot from './components/bot';
+
+function App() {
+  return (
+    <div className="App" style={{minHeight: '100vh', padding: '20px', position: 'relative'}}>
+      <Bot />
+    </div>
+  );
+}
+
+export default App;
